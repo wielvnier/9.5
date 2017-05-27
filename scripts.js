822b4f74a78d2358a802493016a495a545ea0622
wielvnier <filipwielanier@gmail.com>
2017-05-27 09:52:48 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var newElem1 = document.createElement('button');
+var newElem2 = document.createElement('button');
+var newElem3 = document.createElement('button');
+
+newElem1.innerHTML = 'Hello';
+newElem1.innerHTML = 'Hi';
+newElem1.innerHTML = 'Good morning';
+
+newElem1.className += 'button';
+newElem2.className += 'button';
+newElem3.className += 'button';
+
+var buttons = document.getElementsByClassName('button')
+
+function alertElements(elementsNumber) {
+	for (var i = 0; i <= rows; i++){
+
+	}
+
+	alert(buttons)
+}
+
+function alertElements(3)
